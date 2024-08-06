47ec7b7fc722fc41e5d1c7e3387e7a8ac7f9770b
Dao Long Phuoc <dlphuoc16x9@gmail.com>
2024-08-06 20:40:42 +0700

    profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import "./Header.css";
+// import React from "react";
+// import "./Header.css";
 
-const Header = () => {
-  return (
-    <header className="header">
-      <img src="/1a.png" alt="anhs" className="logo" />
-      <div className="header-buttons">
-        <button className="header-btn">Tham gia một trò chơi</button>
-        <button className="header-btn">Đăng nhập</button>
-      </div>
-    </header>
-  );
-};
+// const Header = () => {
+//   return (
+//     <header className="header">
+//       <img src="/1a.png" alt="anhs" className="logo" />
+//       <div className="header-buttons">
+//         <button className="header-btn">Tham gia một trò chơi</button>
+//         <button className="header-btn">Đăng nhập</button>
+//       </div>
+//     </header>
+//   );
+// };
 
-export default Header;
+// export default Header;
---
+++ src/quizzes.jsx
@@ -0,0 +1,53 @@
+const quizzes = [
+  {
+    title: "Vòng quanh thế giới",
+    category: "Khởi động vui vẻ",
+    Image:
+      "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/4bcf5a95-a02f-417d-9cff-49d2c4d10f25?w=400&h=400",
+  },
+
+  {
+    title: "Đúng hay sai",
+    category: "Khởi động vui vẻ",
+    Image:
+      "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/4bcf5a95-a02f-417d-9cff-49d2c4d10f25?w=400&h=400",
+  },
+  {
+    title: "Làm quen với học sinh/sinh viên",
+    category: "Khởi động vui vẻ",
+    Image:
+      "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/4bcf5a95-a02f-417d-9cff-49d2c4d10f25?w=400&h=400",
+  },
+  {
+    title: "Câu đố trước khi lớp bắt đầu",
+    category: "Khởi động vui vẻ",
+    Image:
+      "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/4bcf5a95-a02f-417d-9cff-49d2c4d10f25?w=400&h=400",
+  },
+  {
+    title: "Cái này hay cái kia",
+    category: "Khởi động vui vẻ",
+    Image:
+      "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/4bcf5a95-a02f-417d-9cff-49d2c4d10f25?w=400&h=400",
+  },
+  {
+    title: "Cái này hay cái kia",
+    category: "Khởi động vui vẻ",
+    Image:
+      "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/4bcf5a95-a02f-417d-9cff-49d2c4d10f25?w=400&h=400",
+  },
+  {
+    title: "Cái này hay cái kia",
+    category: "Khởi động vui vẻ",
+    Image:
+      "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/4bcf5a95-a02f-417d-9cff-49d2c4d10f25?w=400&h=400",
+  },
+  {
+    title: "Cái này hay cái kia",
+    category: "Khởi động vui vẻ",
+    Image:
+      "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/4bcf5a95-a02f-417d-9cff-49d2c4d10f25?w=400&h=400",
+  },
+];
+
+export default quizzes;
