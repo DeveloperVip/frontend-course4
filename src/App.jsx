d4bf1ff78ebfcff7f7d60f9c922b23d57d0e52ef
DeverlopVip <hoanganhhung1403@gmail.com>
2024-09-07 16:21:58 +0700

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { privateRoutes, publicRoutes } from "./routes/routes.js";
-import PublicRoutes from "./routes/publicRoutes/publicRoutes.jsx";
+// import PublicRoutes from "./routes/publicRoutes/publicRoutes.jsx";
 import PrivateRoutes from "./routes/privateRoutes/privateRoutes.jsx";
 import Layout from "./components/Layout/Layout.jsx";
+import PublicRoutes from "./routes/publicRoutes/publicRoutes.jsx";
 
 function App() {
   return (
