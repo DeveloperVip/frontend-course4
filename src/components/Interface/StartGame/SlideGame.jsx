e4b4fdf44ec91091b7b94b920ca311454bb1c767
DeverlopVip <hoanganhhung1403@gmail.com>
2024-09-10 17:17:12 +0700

    update image

--- src/components/Interface/StartGame/SlideGame.jsx
+++ src/components/Interface/StartGame/SlideGame.jsx
@@ -36,9 +36,20 @@ const SlideGame = () => {
                 >
                   <div className="text-container w-full">
                     <div className="resizeable gap-x-2 question-text-color text-white">
-                      <p style={{ display: "inline" }}>
-                        {currentQuestion?.question}
-                      </p>
+                      {currentQuestion?.pictureQuestion?.secure_url ? (
+                        <div className="w-full h-full">
+                          <img
+                            className="object-cover overflow-hidden"
+                            src={currentQuestion?.pictureQuestion?.secure_url}
+                            alt=""
+                          />
+                        </div>
+                      ) : (
+                        // <p>{text}</p>
+                        <p style={{ display: "inline" }}>
+                          {currentQuestion?.question}
+                        </p>
+                      )}
                     </div>
                   </div>
                 </div>
